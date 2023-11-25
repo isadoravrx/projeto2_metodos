93ecb14379623a971dd371a53a501f3e72e7a90d
isadoravrx <isavitoriaxavier@gmail.com>
2023-11-25 20:50:35 -0300

    ajustes de lagrange

--- Main_lagrange.java
+++ Main_lagrange.java
@@ -1,14 +1,15 @@
 public class Main_lagrange {
     public static void main(String[] args) {
-        double x[] = {0,5,10};
-        double y[] = {1,0.6875,0.0000}; 
+        double x[] = {1,2,3};
+        double y[] = {1.5,2.5,3.5}; 
 
         Pair pares[] = new Pair[3];
         for(int i = 0; i < 3; i++){
             Pair pair = new Pair<Double>(x[i], y[i]);
             pares[i] = pair;
         }
-        polinomial_lagrange interpo;
+        polinomial_lagrange interpo = new polinomial_lagrange(pares);
+        System.out.println(interpo.interpolacao_lagrange(0.8));
 
     } 
 }
--- polinomial_lagrange.java
+++ polinomial_lagrange.java
@@ -1,17 +1,21 @@
 public class polinomial_lagrange {
-  Pair<Integer> p[];
+  Pair<Double> p[];
 
-  polinomial_lagrange(Pair<Integer> p[]) {
+  polinomial_lagrange(Pair<Double> p[]) {
     this. p = p;
   }
 
-  float interpolacao(float xinterpolado){
-    float l0 = ln(xinterpolado, 0);
-    float l1 = ln(xinterpolado, 1);
-    float l2 = ln(xinterpolado, 2);
-    float l[] = { l0, l1, l2 };
+  int interpolacao_lagrange(double xinterpolado){
+    return (int)interpolacao(xinterpolado);
 
-    float yinterpolado = 0;
+  }
+  double interpolacao(double xinterpolado){
+    double l0 = ln(xinterpolado, 0.0);
+    double l1 = ln(xinterpolado, 1.0);
+    double l2 = ln(xinterpolado, 2.0);
+    double l[] = { l0, l1, l2 };
+
+    double yinterpolado = 0;
     for (int j = 0; j < 3; j++) {
       yinterpolado += p[j].y * l[j];
     }
@@ -19,12 +23,13 @@ public class polinomial_lagrange {
     return yinterpolado;
   }
 
-  float ln(float x, float l) {
+  double ln(double x, double l) {
     if (l == 0) {
-      return ((float) ((x - p[1].x) * (x - p[2].x))) / ((float) ((p[0].x - p[1].x) * (p[0].x - p[2].x)));
+      return ((double)((x - p[1].x) * (x - p[2].x))) / ((double)((p[0].x - p[1].x) * (p[0].x - p[2].x)));
     } else if (l == 1) {
-      return ((float) ((x - p[0].x) * (x - p[2].x))) / ((float) ((p[1].x - p[0].x) * (p[1].x - p[2].x)));
+      return ((double) ((x - p[0].x) * (x - p[2].x))) / ((double) ((p[1].x - p[0].x) * (p[1].x - p[2].x)));
     }
-    return ((float) ((x - p[0].x) * (x - p[1].x))) / ((float) ((p[2].x - p[0].x) * (p[2].x - p[1].x)));
+    
+    return ((double) ((x - p[0].x) * (x - p[1].x))) / ((double) ((p[2].x - p[0].x) * (p[2].x - p[1].x)));
   }
 }
